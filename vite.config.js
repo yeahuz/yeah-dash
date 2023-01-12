6093807f18e4283ede4dcc3617a182b13eeac98e
Owner <owner@Owners-MacBook-Pro-2.local>
2023-01-12 17:20:28 +0500

    chore: fixed configs;

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [preact()],
-  resolve: {
-    alias: {
-      react: 'preact/compat'
-    }
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000"
+
+export default defineConfig(() => {
+  return {
+    plugins: [preact()],
+    resolve: {
+      alias: {
+        react: 'preact/compat'
+      }
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:3001"
+        }
       }
     }
-  }
-})
+  };
+});
